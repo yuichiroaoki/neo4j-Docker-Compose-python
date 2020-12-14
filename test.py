1be1acacd637b7d8c8fb3832a37f4bc2c1d0baed
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-12-14 17:30:14 +0900

    change test.py

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
 import subprocess
 
-# p1 = subprocess.run(['ls', '-la', 'the'], 
-#     capture_output=True, text=True, check=True)
-
 p2 = subprocess.run(['docker-compose', 'up', '-d'],
     capture_output=True, text=True, check=True)
-
-# shows 0 if there no error
-# print(p1.returncode)
-
-# print(p1.stdout)
 
 if p2.returncode != 0:
     print(p1.stderr)
