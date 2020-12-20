ba7cbe07d5b05b692f2f7aa5d86743c103ce816c
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-12-20 16:00:33 +0900

    change fileInfo.py

--- fileInfo.py
+++ fileInfo.py
@@ -1,6 +1,14 @@
 from os import listdir
 from os.path import isfile, join
 
-mypath = "./import"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print(onlyfiles)
+class FileManager():
+    
+    def list_files(self):
+        mypath = "./import"
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        print(onlyfiles)
+
+
+examples = FileManager()
+
+print(examples.list_files())
