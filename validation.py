fd7268d4f48fd75d629c43b06d314fe3b693da6e
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-12-20 23:00:00 +0900

    add func check_if_source_target_are_valid

--- validation.py
+++ validation.py
@@ -26,6 +26,40 @@ class GraphData():
             print("ID should be unique")
             return False
 
+    def check_if_source_target_are_valid(self, graphData):
+
+        def get_node_list(graphData):
+            node_list = []
+            for data in graphData:
+                try:
+                    if data["position"]:
+                        node_list.append(data["data"]["id"])
+                except KeyError:
+                    continue
+                except Exception as e:
+                    raise e
+
+            return node_list
+
+        def if_the_edge_is_valid(graphData, nodeList):
+            for data in graphData:
+                try:
+                    if data["data"]["source"] and data["data"]["target"]:
+                        edge_id = data["data"]["id"]
+                        source = data["data"]["source"]
+                        target = data["data"]["target"]
+                
+                        if not source in nodeList:
+                            raise Exception("Can not create edge {} with nonexistant source {}".format(edge_id, source))
+                        if not target in nodeList:
+                            raise Exception("Can not create edge {} with nonexistant target {}".format(edge_id, target))
+                except KeyError:
+                    continue
+                except Exception as e:
+                    raise e
+        
+        if_the_edge_is_valid(graphData, get_node_list(graphData))
+
 uploaded_data = [
     {"data": {"id": "254126",
                 "label": "254126"
@@ -35,14 +69,56 @@ uploaded_data = [
                 "y": 420.89410400390625
             }
     },{"data": {"source": "244521", "target": "280038", "id": '280038'}},
+    {
+        "data": {
+            "id": "280038",
+            "label": "280038"
+        },
+        "position": {
+            "x": -459,
+            "y": 234
+        }
+    },
     {"data": {"source": "244521", "target": "280038", "id": "244521:d"}},
     {"data": {"source": "244521", "target": "280038", "id": "244521:d"}}
 ]
+correct_data = [
+    {
+        "data": {
+            "id": "254126",
+            "label": "254126"
+        },
+        "position": {
+            "x": -269.32,
+            "y": 420.33
+        }
+    },
+    {
+        "data": {
+            "id": "280038",
+            "label": "280038"
+        },
+        "position": {
+            "x": -459.32,
+            "y": 234.3
+        }
+    },
+    {
+        "data": {
+            "source": "254126",
+            "target": "254126",
+            "id": "254126:280038"
+        }
+    }
+]
 
 example = GraphData()
-if example.check_data_type(uploaded_data):
-    print(example.check_data_type(uploaded_data))
+if example.check_data_type(correct_data):
+    print(example.check_data_type(correct_data))
 
 if example.check_if_the_id_is_unique(uploaded_data):
     print(example.check_if_the_id_is_unique(uploaded_data))
+
+if example.check_if_source_target_are_valid(uploaded_data):
+    print(example.check_if_source_target_are_valid(uploaded_data))
 
