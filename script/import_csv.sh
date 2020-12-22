bae267fb33f681c56d36a7b820ff95fb99de141d
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-12-22 15:05:59 +0900

    change docker-compose

--- script/import_csv.sh
+++ script/import_csv.sh
@@ -4,7 +4,7 @@ set -eu
 # CSVインポート
 echo "importing csv started."
 /var/lib/neo4j/bin/neo4j-admin import \
-  --database testdata \
+  --database start \
   --nodes import/nodes.csv \
   --relationships import/relations.csv 
 echo "importing csv finished."
